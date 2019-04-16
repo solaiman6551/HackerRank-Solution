7d9c2c02b7c44091597d2f69ad6140b64a72e386
solaiman6551 <31163258+solaiman6551@users.noreply.github.com>
2019-04-17 01:13:16 +0600

    Add files via upload

---
+++ Insert a Node at the Tail of a Linked List.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+      
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtTail function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+            SinglyLinkedListNode n_node = new SinglyLinkedListNode(data);
+            if(head == null){
+                head = n_node;
+                return head;
+            }
+            
+            SinglyLinkedListNode c_node = head;
+            while(c_node.next != null){
+                c_node = c_node.next;
+            }
+
+            c_node.next = n_node;
+             return head;
+        }
+
+    
+
+    private static final Scanner scanner = new Scanner(System.in);
